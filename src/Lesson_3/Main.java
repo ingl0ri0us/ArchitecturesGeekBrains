fa51bea00fa1cd7398ca21e2fe0953358321dc20
ingl0ri0us <rinat.y@protonmail.com>
2020-04-21 12:02:53 +0300

    Lesson_3

---
+++ src/Lesson_3/Main.java
@@ -0,0 +1,22 @@
+package Lesson_3;
+
+public class Main {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts
+                .Builder("Coca-Cola", 240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbohydrate(27)
+                .build();
+
+        NutritionFacts snickers = new NutritionFacts
+                .Builder("Snickers", 300, 10)
+                .calories(200)
+                .fat(15)
+                .carbohydrate(100)
+                .build();
+
+        System.out.println(cocaCola.toString());
+        System.out.println(snickers.toString());
+    }
+}
